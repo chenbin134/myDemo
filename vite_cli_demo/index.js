27a3e1e0727f32955afd6a7c6f33ccc61f177dc1
bin_cb <bin_cb@163.com>
2021-06-30 19:02:35 +0800

    update cli

--- vite_cli_demo/index.js
+++ vite_cli_demo/index.js
@@ -9,6 +9,7 @@ const {
   magenta,
   lightRed,
   red,
+  stripColors
 } = require('kolorist');
 
 
@@ -96,18 +97,72 @@ async function init(){
     }
 
   }
+
   // 确定模版名称
-  // let { t } = await prompt({
-  //   type: 
-  // }) 
+  if(!template || !TEMPLATES.includes(template)) {
+    let { t } = await prompt({
+      type:'select',
+      name: "t",
+      message: "Select a template:",
+      choices: TEMPLATES
+    })
+    template = stripColors(t);
+  }
+
+  const templateDir = path.join(__dirname,`template-${template}`);
+
+  // 开始将模版内容写入到创建的文件夹中
+  
+  for(const file of fs.readdirSync(templateDir)){
+
+    copy(path.join(templateDir,file),path.join(root,file));
+
+  }
+  
 
 }
 
 /**
  * 清空指定目录下的文件
+ * @param dir 要清空目录的绝对地址
  */
 function emptyDir(dir){
-  console.log(red('clear'))
+  if(!fs.existsSync(dir)) {
+    return;
+  }
+  for(const file of fs.readdirSync(dir)) {
+    const abs = path.resolve(dir,file);
+    if(fs.lstatSync(abs).isDirectory()){
+      emptyDir(abs);
+      fs.rmdirSync(abs);
+    } else {
+      fs.unlinkSync(abs);
+    }
+  }
+}
+
+/**
+ * 复制文件(夹)
+ * @param src 目标文件
+ * @param dest 当前位置
+ */
+function copy(src,dest){
+  const stat = fs.statSync(src);
+  if(stat.isDirectory()){
+    copyDir(src,dest);
+  } else {
+    fs.copyFileSync(src,dest);
+  }
+}
+
+function copyDir(srcDir,destDir){
+  fs.mkdirSync(destDir,{ recursive: true });
+  for (const file of fs.readdirSync(srcDir)) {
+    const srcFile = path.resolve(srcDir, file)
+    const destFile = path.resolve(destDir, file)
+    copy(srcFile, destFile)
+  }
+
 }
 
 init();
