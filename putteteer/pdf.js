58c50c66927faf69d05b13600755ba78336ee6f6
bin_cb <bin_cb@163.com>
2022-01-06 16:16:18 +0800

    update

--- putteteer/pdf.js
+++ putteteer/pdf.js
@@ -6,7 +6,7 @@ const puppeteer = require('puppeteer');
     // headless: false,调试
   });
   const page = await browser.newPage();
-  await page.goto('https://jelly.jd.com/article/604a15ce9c61f9014c2195ed', {
+    await page.goto('https://jelly.jd.com/article/604a15ce9c61f9014c2195ed', {
     waitUntil: 'networkidle2',
   });
   await page.pdf({ path: 'hn.pdf', format: 'a4' });
